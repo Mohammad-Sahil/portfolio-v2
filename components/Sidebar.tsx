9797243971df9bb99aa98099b8fde00ac9b88eef
Mohammad Sahil <sahilmejakhas@gmail.com>
2026-01-26 19:53:33 +0530

    photo modified

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../styles/Sidebar.module.css'
-import profileImg from '../public/profilee.png'
+import profileImg from '../public/profile.jpg'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import GitHubIcon from '@mui/icons-material/GitHub'
